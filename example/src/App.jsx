72468117e609771a98a5c08149a69ae01ab2b2f9
Ptolemy Henson <ptolemy2002@gmail.com>
2024-07-24 12:18:36 -0400

    Setup the Library

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,11 +1,40 @@
-import {Example} from "@local/lib";
+import { ErrorBoundary, useErrorBoundary } from "react-error-boundary";
+import useManualErrorHandling from "@ptolemy2002/react-manual-error-handling";
+
+async function wait(ms) {
+    return new Promise((_, reject) => {
+        setTimeout(() => reject(new Error("Random error")), ms);
+    });
+}
 
 function App() {
-  return (
-    <div className="App">
-      <Example />
-    </div>
-  );
+    return (
+      <div className="App">
+        <ErrorBoundary fallback={<ErrorNotice />}>
+            <Main />
+        </ErrorBoundary>
+      </div>
+    );
 }
 
 export default App;
+
+function Main() {
+    const { _try } = useManualErrorHandling();
+    const handleClick = async () => _try(() => wait(1000));
+
+    return (
+        <button onClick={handleClick}>Click to throw an error after 1 second</button>
+    );
+}
+
+function ErrorNotice() {
+    const {resetBoundary} = useErrorBoundary();
+  
+    return (
+        <div className="alert alert-danger">
+            <p>An error occurred.</p>
+            <button onClick={resetBoundary}>Try again</button>
+        </div>
+    );
+}
